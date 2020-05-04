0ccb00c8b3c69a90d397f197e4b777b6c1cb1842
Anton Ivanov <anton.ivanov@cambridgegreys.com>
2020-05-04 10:46:05 +0100

    Further fixes
    Signed-off-by: Anton Ivanov <anton.ivanov@cambridgegreys.com>

--- lib/ovn-util.c
+++ lib/ovn-util.c
@@ -475,9 +475,19 @@ ovn_destroy_tnlids(struct hmap *tnlids)
 void
 ovn_destroy_local_update_global_tnlids(struct hmap *local, struct hmap *global)
 {
-    struct tnlid_node *node;
+    struct tnlid_node *node, *gnode;
     HMAP_FOR_EACH_POP (node, hmap_node, local) {
-        hmap_remove(global, &node->hmap_node);
+        bool to_remove = false;
+
+        HMAP_FOR_EACH_IN_BUCKET (gnode, hmap_node, hash_int(node->tnlid, 0), global) {
+            if (gnode->tnlid == node->tnlid) {
+                to_remove = true;
+                break;
+            }
+        }
+        if (to_remove) {
+            hmap_remove(global, &gnode->hmap_node);
+        }
         free(node);
     }
     hmap_destroy(local);
